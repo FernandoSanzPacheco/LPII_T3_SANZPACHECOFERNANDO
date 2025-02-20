91863324b232b74b78144a91e8aae9aba7f56ca6
Fernando <fersanzpa@gmail.com>
2025-02-19 20:32:58 -0500

    version 1.0

---
+++ LPII_T3_SANZPACHECOFERNANDO/src/main/java/com/bd/medico2025/LpiiT3SanzpachecofernandoApplication.java
@@ -0,0 +1,13 @@
+package com.bd.medico2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LpiiT3SanzpachecofernandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LpiiT3SanzpachecofernandoApplication.class, args);
+	}
+
+}
